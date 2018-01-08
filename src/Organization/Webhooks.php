4cde02a5c3616195ee6a125ded7fd15540d6a536
Erwin Deckers <e.deckers@hypotheekbond.nl>
2018-01-08 08:50:36 +0100

    Webhooks put and post

--- src/Organization/Webhooks.php
+++ src/Organization/Webhooks.php
@@ -54,11 +54,77 @@ final class Webhooks
      *
      * @return array
      */
+    public function post(Token $accessToken, string $targetUri, string $identifier)
+    {
+        $data = [
+            'target_uri' => $targetUri,
+            'identifier' => $identifier,
+        ];
+        try {
+            $httpResponse =
+                $this->guzzleClient->post(
+                    $this->config->getApiHost() . 'api/webhooks/webhook',
+                    [
+                        'headers' => [
+                            'Accept'        => 'application/json',
+                            'Authorization' => 'Bearer ' . (string) $accessToken,
+                        ],
+                        'form_params' => $data
+                    ])->getBody()->getContents();
+        } catch (BadResponseException $e) {
+            throw new InvalidRequestException(
+                $e->getResponse() ? $e->getResponse()->getBody()->getContents() : 'An unknown error has occurred',
+                $e->getResponse() ? $e->getResponse()->getStatusCode() : 0
+            );
+        }
+
+        return json_decode($httpResponse);
+    }
+
+    /**
+     * @param Token $accessToken
+     * @param int   $id
+     *
+     * @return array
+     */
     public function get(Token $accessToken, int $id)
     {
         try {
             $httpResponse =
                 $this->guzzleClient->get($this->config->getApiHost() . 'api/webhooks/webhook/' . $id,
+                    [
+                        'headers' => [
+                            'Accept'        => 'application/json',
+                            'Authorization' => 'Bearer ' . (string) $accessToken,
+                        ],
+                    ])->getBody()->getContents();
+        } catch (BadResponseException $e) {
+            throw new InvalidRequestException(
+                $e->getResponse() ? $e->getResponse()->getBody()->getContents() : 'An unknown error has occurred',
+                $e->getResponse() ? $e->getResponse()->getStatusCode() : 0
+            );
+        }
+
+        return json_decode($httpResponse);
+    }
+
+    /**
+     * @param Token $accessToken
+     * @param int   $id
+     *
+     * @return array
+     */
+    public function put(Token $accessToken, int $id, string $targetUri, string $identifier)
+    {
+        $query = [
+            'target_uri' => $targetUri,
+            'identifier' => $identifier,
+        ];
+        $queryString = http_build_query($query);
+        try {
+            $httpResponse =
+                $this->guzzleClient->put(
+                    $this->config->getApiHost() . 'api/webhooks/webhook/' . $id . '?' . $queryString,
                     [
                         'headers' => [
                             'Accept'        => 'application/json',
