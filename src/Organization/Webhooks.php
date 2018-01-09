54f47b980f043e7906740168de9c229bcc6e612d
Erwin Deckers <e.deckers@hypotheekbond.nl>
2018-01-09 08:54:46 +0100

    Webhook response

--- src/Organization/Webhooks.php
+++ src/Organization/Webhooks.php
@@ -5,7 +5,9 @@ namespace Ufo\Client\Organization;
 
 use GuzzleHttp\Client as GuzzleClient;
 use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Psr7\Response;
 use Lcobucci\JWT\Token;
+use Psr\Http\Message\ServerRequestInterface;
 use Ufo\Client\Exception\InvalidRequestException;
 
 final class Webhooks
@@ -65,11 +67,11 @@ final class Webhooks
                 $this->guzzleClient->post(
                     $this->config->getApiHost() . 'api/webhooks/webhook',
                     [
-                        'headers' => [
+                        'headers'     => [
                             'Accept'        => 'application/json',
                             'Authorization' => 'Bearer ' . (string) $accessToken,
                         ],
-                        'form_params' => $data
+                        'form_params' => $data,
                     ])->getBody()->getContents();
         } catch (BadResponseException $e) {
             throw new InvalidRequestException(
@@ -166,5 +168,34 @@ final class Webhooks
         }
 
         return json_decode($httpResponse);
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param string                 $secret
+     * @param callable|null          $callable
+     *
+     * @return Response
+     */
+    public function receive(ServerRequestInterface $request, string $secret, callable $callable = null)
+    {
+        $digestable = $request->getBody()->getContents() . $secret;
+        $digest = hash_hmac('sha256', $digestable, $secret);
+        if ($request->getHeader('X-Hook-Signature') === $digest) {
+            $data = json_decode($request->getBody()->getContents());
+            $callable($data);
+            return new Response(
+                200,
+                [
+                    'X-Hook-Secret' => $secret,
+                ],
+                'received');
+        }
+
+        return new Response(
+            400,
+            [],
+            'Invalid signature'
+        );
     }
 }
