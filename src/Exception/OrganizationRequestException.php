8c5afb0c23b6f7b4637337d99e2faaa76429010c
Erwin Deckers <e.deckers@hypotheekbond.nl>
2018-04-30 13:26:32 +0200

    Differentiates consumer connection and organization connection issues.

---
+++ src/Exception/OrganizationRequestException.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types = 1);
+
+namespace Ufo\Client\Exception;
+
+/**
+ * Class OrganizationRequestException
+ */
+class OrganizationRequestException extends \RuntimeException
+{
+}
