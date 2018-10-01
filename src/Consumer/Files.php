387589baf9e12f9c05681dce0b8e1c7de42dd141
Erwin Deckers <e.deckers@hypotheekbond.nl>
2018-10-01 09:37:52 +0200

    [IP1-1167] Adds Files

---
+++ src/Consumer/Files.php
@@ -0,0 +1,70 @@
+<?php
+declare(strict_types = 1);
+
+namespace Ufo\Client\Consumer;
+
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\RequestOptions;
+use Lcobucci\JWT\Token;
+use Ufo\Client\Organization\Config;
+use Ufo\Client\Traits\ProcessesBadResponses;
+
+/**
+ * Class Files
+ */
+final class Files
+{
+    use ProcessesBadResponses;
+    /** @var GuzzleClient */
+    private $guzzleClient;
+    /** @var Config */
+    private $config;
+
+    /**
+     * Dossier constructor.
+     *
+     * @param Config       $config
+     * @param GuzzleClient $guzzleClient
+     */
+    public function __construct(
+        Config $config,
+        GuzzleClient $guzzleClient
+    ) {
+        $this->guzzleClient = $guzzleClient;
+        $this->config = $config;
+    }
+
+    /**
+     * @param Token  $accessToken
+     * @param string $consumerId
+     *
+     * @return array
+     */
+    public function list(
+        Token $accessToken,
+        string $consumerId
+    ): array {
+        $query = [
+            'consumer_id' => $consumerId,
+        ];
+        $queryString = http_build_query($query);
+        try {
+            $httpResponse =
+                $this->guzzleClient->get(
+                    $this->config->getApiHost() . '/files?' . $queryString,
+                    [
+                        RequestOptions::HEADERS => [
+                            'Accept'        => 'application/json',
+                            'Authorization' => 'Bearer ' . (string) $accessToken,
+                        ],
+                    ]
+                )->getBody()->getContents();
+        } catch (BadResponseException $e) {
+            $this->processBadResponse($e);
+        }
+
+        /** @noinspection PhpUndefinedVariableInspection */
+        return json_decode($httpResponse, true);
+    }
+}
