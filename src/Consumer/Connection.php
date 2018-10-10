1442fd2e3a509c1cafccf1099fd4f85324277355
Erwin Deckers <e.deckers@hypotheekbond.nl>
2018-10-10 12:10:52 +0200

    [IP1-1167] update and test Connection class

--- src/Consumer/Connection.php
+++ src/Consumer/Connection.php
@@ -3,76 +3,41 @@ declare(strict_types = 1);
 
 namespace Ufo\Client\Consumer;
 
+use Ramsey\Uuid\UuidInterface;
+
 /**
  * Class Connection
  */
 final class Connection
 {
-    /** @var string */
-    private $organizationConsumerId;
-    /** @var string */
+    /** @var UuidInterface */
     private $ufoConsumerId;
     /** @var array|null */
     private $grantedScopes;
-    /** @var null|string */
-    private $connectionCode1;
-    /** @var null|string */
-    private $connectionCode2;
 
     /**
      * Connection constructor.
      *
-     * @param string      $organizationConsumerId
-     * @param string      $ufoConsumerId
-     * @param string|null $connectionCode1
-     * @param string|null $connectionCode2
-     * @param array|null  $grantedScopes
+     * @param UuidInterface $ufoConsumerId
+     * @param array|null    $grantedScopes
      */
     public function __construct(
-        string $organizationConsumerId,
-        string $ufoConsumerId,
-        string $connectionCode1 = null,
-        string $connectionCode2 = null,
+        UuidInterface $ufoConsumerId,
         array $grantedScopes = null
     ) {
-        $this->organizationConsumerId = $organizationConsumerId;
         $this->ufoConsumerId = $ufoConsumerId;
-        $this->connectionCode1 = $connectionCode1;
-        $this->connectionCode2 = $connectionCode2;
         $this->grantedScopes = $grantedScopes;
     }
 
-    /**
-     * @return string
-     */
-    public function getOrganizationConsumerId(): string
-    {
-        return $this->organizationConsumerId;
-    }
 
     /**
-     * @return string
+     * @return UuidInterface
      */
-    public function getUfoConsumerId(): string
+    public function getUfoConsumerId(): UuidInterface
     {
         return $this->ufoConsumerId;
     }
 
-    /**
-     * @return null|string
-     */
-    public function getConnectionCode1()
-    {
-        return $this->connectionCode1;
-    }
-
-    /**
-     * @return null|string
-     */
-    public function getConnectionCode2()
-    {
-        return $this->connectionCode2;
-    }
 
     /**
      * @return array|null
