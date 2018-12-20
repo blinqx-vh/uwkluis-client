c2b9be21f1d77e38c3a23eaff1d8ee6b53fff0f0
Artur Kupczak <a.kupczak@hypotheekbond.nl>
2018-12-20 09:52:55 +0100

    IP1-1825: Added index and get

--- src/Consumer/DocumentRequest.php
+++ src/Consumer/DocumentRequest.php
@@ -5,6 +5,9 @@ namespace Ufo\Client\Consumer;
 
 
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\RequestOptions;
+use Lcobucci\JWT\Token;
 use Ufo\Client\Organization\Config;
 use Ufo\Client\Traits\ProcessesBadResponses;
 
@@ -30,4 +33,76 @@ class DocumentRequest
         $this->guzzleClient = $guzzleClient;
         $this->config = $config;
     }
+
+
+    /**
+     * @param Token $accessToken
+     * @param string $consumerId
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function index(
+        Token $accessToken,
+        string $consumerId
+    )
+    {
+        $queryString = http_build_query([
+            'consumer_id' => $consumerId,
+        ]);
+
+        try {
+            $httpResponse = $this->guzzleClient->request(
+                'get',
+                "{$this->config->getApiHost()}/document-request?{$queryString}",
+                [
+                    RequestOptions::HEADERS => [
+                        'Accept'        => 'application/json',
+                        'Authorization' => 'Bearer ' . (string) $accessToken,
+                    ],
+                ]
+            )->getBody()->getContents();
+        } catch (BadResponseException $e) {
+            $this->processBadResponse($e);
+        }
+
+        /** @noinspection PhpUndefinedVariableInspection */
+        return json_decode($httpResponse, true);
+    }
+
+    /**
+     * @param Token $accessToken
+     * @param string $consumerId
+     * @param string $documentId
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function get(
+        Token $accessToken,
+        string $consumerId,
+        string $documentId
+    )
+    {
+        $queryString = http_build_query([
+            'consumer_id' => $consumerId,
+        ]);
+
+        try {
+            $httpResponse = $this->guzzleClient->request(
+                'get',
+                "{$this->config->getApiHost()}/document-request/{$documentId}?{$queryString}",
+                [
+                    RequestOptions::HEADERS => [
+                        'Accept'        => 'application/json',
+                        'Authorization' => 'Bearer ' . (string) $accessToken,
+                    ],
+                ]
+            )->getBody()->getContents();
+        } catch (BadResponseException $e) {
+            $this->processBadResponse($e);
+        }
+
+        /** @noinspection PhpUndefinedVariableInspection */
+        return json_decode($httpResponse, true);
+    }
+
 }
