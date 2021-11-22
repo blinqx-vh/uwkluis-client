3c5e0c64f6ab72a7cf1bf2d74ee242ffa36e7426
Jaap Redeker <j.redeker@hypotheekbond.nl>
2021-11-22 15:22:45 +0100

    added associative

--- src/Traits/ProcessesBadResponses.php
+++ src/Traits/ProcessesBadResponses.php
@@ -42,7 +42,7 @@ trait ProcessesBadResponses
             }
 
             if ($e->getCode() === StatusCodeInterface::STATUS_UNPROCESSABLE_ENTITY) {
-                $errors = json_decode($exceptionResponse['message']);
+                $errors = json_decode($exceptionResponse['message'], true);
                 if (json_last_error() !== JSON_ERROR_NONE) {
                     $errors = [$exceptionResponse['message']];
                 }
