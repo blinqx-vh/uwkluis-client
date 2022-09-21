f64bd11ecba6bac84b79f24446c1cf5a50e647d1
Albert de Boer <a.deboer@hypotheekbond.nl>
2022-09-21 11:53:06 +0200

    IT-9170: Add source data requests to client

---
+++ src/Consumer/SourceDataRequest.php
@@ -0,0 +1,141 @@
+<?php
+declare(strict_types = 1);
+
+namespace UwKluis\Client\Consumer;
+
+use Fig\Http\Message\RequestMethodInterface;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\RequestOptions;
+use Lcobucci\JWT\Token;
+use UwKluis\Client\Organization\Config;
+use UwKluis\Client\Traits\ProcessesBadResponses;
+
+class SourceDataRequest
+{
+    use ProcessesBadResponses;
+    private ClientInterface $guzzleClient;
+    private Config $config;
+
+    public function __construct(
+        Config $config,
+        ClientInterface $guzzleClient
+    ) {
+        $this->guzzleClient = $guzzleClient;
+        $this->config = $config;
+    }
+
+    public function list(
+        Token $accessToken,
+        string $consumerId
+    ): array {
+        $queryString = http_build_query([
+            'consumer_id' => $consumerId,
+        ]);
+
+        try {
+            $httpResponse = $this->guzzleClient->request(
+                RequestMethodInterface::METHOD_GET,
+                "{$this->config->getApiHost()}/source-data-request?{$queryString}",
+                [
+                    RequestOptions::HEADERS => [
+                        'Accept'        => 'application/json',
+                        'Authorization' => 'Bearer ' . $accessToken->toString(),
+                    ],
+                ]
+            )->getBody()->getContents();
+        } catch (BadResponseException $e) {
+            $this->processBadResponse($e);
+        }
+
+        return json_decode($httpResponse, true);
+    }
+
+    public function get(
+        Token $accessToken,
+        string $consumerId,
+        string $sourceDataRequestId
+    ): array {
+        $queryString = http_build_query([
+            'consumer_id' => $consumerId,
+        ]);
+
+        try {
+            $httpResponse = $this->guzzleClient->request(
+                RequestMethodInterface::METHOD_GET,
+                "{$this->config->getApiHost()}/source-data-request/{$sourceDataRequestId}?{$queryString}",
+                [
+                    RequestOptions::HEADERS => [
+                        'Accept'        => 'application/json',
+                        'Authorization' => 'Bearer ' . $accessToken->toString(),
+                    ],
+                ]
+            )->getBody()->getContents();
+        } catch (BadResponseException $e) {
+            $this->processBadResponse($e);
+        }
+
+        return json_decode($httpResponse, true);
+    }
+
+    public function delete(
+        Token $accessToken,
+        string $consumerId,
+        int $sourceDataRequestId
+    ): bool {
+        $queryString = http_build_query([
+            'consumer_id' => $consumerId,
+        ]);
+
+        try {
+            $httpResponse = $this->guzzleClient->request(
+                RequestMethodInterface::METHOD_DELETE,
+                "{$this->config->getApiHost()}/source-data-request/{$sourceDataRequestId}?{$queryString}",
+                [
+                    RequestOptions::HEADERS => [
+                        'Accept'        => 'application/json',
+                        'Authorization' => 'Bearer ' . $accessToken->toString(),
+                    ],
+                ]
+            );
+        } catch (BadResponseException $e) {
+            $this->processBadResponse($e);
+        }
+
+        return $httpResponse->getStatusCode() === 200;
+    }
+
+    public function create(
+        Token $accessToken,
+        string $consumerId,
+        string $title,
+        string $description,
+        string $sourceType
+    ): array {
+        $queryString = http_build_query([
+            'consumer_id' => $consumerId,
+        ]);
+
+        try {
+            $httpResponse = $this->guzzleClient->request(
+                RequestMethodInterface::METHOD_POST,
+                "{$this->config->getApiHost()}/source-data-request/?{$queryString}",
+                [
+                    RequestOptions::HEADERS => [
+                        'Accept'        => 'application/json',
+                        'Authorization' => 'Bearer ' . $accessToken->toString(),
+                    ],
+                    RequestOptions::FORM_PARAMS => [
+                        'title' => $title,
+                        'description' => $description,
+                        'source_type' => $sourceType,
+                    ]
+                ]
+            )->getBody()->getContents();
+        } catch (BadResponseException $e) {
+            $this->processBadResponse($e);
+        }
+
+        return json_decode($httpResponse, true);
+    }
+}
