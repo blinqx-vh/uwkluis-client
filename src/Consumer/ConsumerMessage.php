d20bbe182ff295d3e922047758842befd9654f16
jaapredeker <59443178+jaapredeker@users.noreply.github.com>
2023-03-21 11:09:10 +0100

    added get endpoint for consumer messages

---
+++ src/Consumer/ConsumerMessage.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types = 1);
+
+namespace UwKluis\Client\Consumer;
+
+use Fig\Http\Message\RequestMethodInterface;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\RequestOptions;
+use Lcobucci\JWT\Token;
+use UwKluis\Client\Organization\Config;
+use UwKluis\Client\Traits\ProcessesBadResponses;
+
+final class ConsumerMessage
+{
+    use ProcessesBadResponses;
+
+    private ClientInterface $guzzleClient;
+    private Config $config;
+
+    public function __construct(
+        Config $config,
+        ClientInterface $guzzleClient
+    ) {
+        $this->guzzleClient = $guzzleClient;
+        $this->config = $config;
+    }
+
+    public function get(
+        Token $accessToken,
+        string $consumerId,
+        string $consumerMessageUuid
+    ): array {
+        $queryString = http_build_query([
+            'consumer_id' => $consumerId,
+        ]);
+
+        try {
+            $httpResponse = $this->guzzleClient->request(
+                RequestMethodInterface::METHOD_GET,
+                "{$this->config->getApiHost()}/consumer-messages/{$consumerMessageUuid}?{$queryString}",
+                [
+                    RequestOptions::HEADERS => [
+                        'Accept' => 'application/json',
+                        'Authorization' => 'Bearer ' . $accessToken->toString(),
+                    ],
+                ]
+            )->getBody()->getContents();
+        } catch (BadResponseException $e) {
+            $this->processBadResponse($e);
+        }
+
+        return json_decode($httpResponse, true);
+    }
+}
